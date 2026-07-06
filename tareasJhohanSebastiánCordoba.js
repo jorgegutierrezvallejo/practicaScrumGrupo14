2628ee3310b5b15635488dfef3757bf182f727a8
JhohanSebastianCordobaP <scba246@gmail.com>
2026-07-06 16:18:31 -0500

    agrego actividades del spring

---
+++ tareasJhohanSebastiánCordoba.js
@@ -0,0 +1,12 @@
+// Jhohan Sebastián Cordoba
+
+// Rol: Backend / Base de datos
+
+// Crear estructura de arquitectura MVC. 
+// Diseñar la base de datos. 
+// Crear la base de datos funcional. 
+// Preparar el servidor. 
+// Crear la lógica del Fetch para los posts. 
+// Crear repositorio en GitHub. 
+// Crear categorías para los posts del blog. 
+// Apoyar en la corrección de errores técnicos.
