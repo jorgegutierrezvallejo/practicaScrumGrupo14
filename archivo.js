3a51f9bd6817d0e01143bd986f3e04e5e76a77ac
Sebsatian Pereira <sebasperemald04@gmail.com>
2026-07-06 16:30:57 -0500

    Archivo de tareas - Sebastian Pereira

---
+++ archivo.js
@@ -0,0 +1,17 @@
+/*
+Sebastian Pereira
+
+Rol: UX/UI / Contenido / Scrum Master
+
+Diseñar pantallas en Figma. 
+Diseñar UX & UI general del proyecto. 
+Definir presupuesto del proyecto. 
+Crear contenido semana 1. 
+Crear contenido semana 2. 
+Definir historias de usuario. 
+Crear documento de entrega o README del proyecto. 
+Coordinar reuniones Scrum. 
+Preparar presentación final del proyecto junto con el equipo.
+
+
+*/
