f31127f114687904ee473cb051d578be55bc6973
JorgeGutierrezVallejo <jegv08@gmail.com>
2026-07-06 16:20:03 -0500

    Agrega documento Sprint y actualiza tareas Game Zone

--- scrumGameZone.js
+++ scrumGameZone.js
@@ -1,1 +1,12 @@
-Scrum 
+Tareas por participante – Proyecto Game Zone
+Jorge Gutiérrez
+
+Rol: Frontend / Maquetación
+
+
+
+Rol: Backend / Base de datos
+
+C
+
+Rol: UX/UI / Contenido / Scrum Master
